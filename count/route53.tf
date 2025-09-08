85737b9344fc325865263f9536f7140d9bfa3ea1
Mounika <mounikamethuku95@gmail.com>
2025-09-09 00:20:26 +0530

    created public r53 record

--- count/route53.tf
+++ count/route53.tf
@@ -1,8 +1,15 @@
 resource "aws_route53_record" "this" {
-    count = 3
     zone_id = var.zone_id
     name = "${var.instances[count.index]}.${var.domain_name}" #interpolation-concatination of text and variables
     type = "A"
     ttl = 1
     records = [aws_instance.this[count.index].private_ip]
+}
+
+resource "aws_route53_record" "this" {
+    zone_id = var.zone_id
+    name = "${var.instances[2]}.${var.domain_name}" #interpolation-concatination of text and variables
+    type = "A"
+    ttl = 1
+    records = [aws_instance.this[2].public_ip]
 }
