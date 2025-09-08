d82678c2b4cdbe2a41cd21bad74a24658bc24282
Mounika <mounikamethuku95@gmail.com>
2025-09-08 09:01:52 +0530

    added ingress & egress

--- security-group.tf
+++ security-group.tf
@@ -2,6 +2,20 @@ resource "aws_security_group" "allow_tls" {
     name    = "allow_tls"
     description     = "Allow all inbound and outbound traffic"
 
+    ingress {
+        from_port = 22
+        to_port   = 22
+        protocol  ="tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port   = 0
+        protocol  ="-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
     tags = {
         Name = "allow_tls"
     }
